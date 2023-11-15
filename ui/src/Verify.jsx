a8e3fa93f94fa0e7a6f0f475a30ab09e91e58305
shriekster <shrieksterr@gmail.com>
2023-11-15 14:00:54 +0200

    getting started with the UI

---
+++ ui/src/Verify.jsx
@@ -0,0 +1,11 @@
+import Slider from '@mui/material/Slider';
+
+import robotImage from './assets/robot.png';
+
+export default function Verify() {
+
+    return (
+        <img src={robotImage} width={100} height={100}/>
+    );
+
+}
--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import App from './App.jsx'
-import './index.css'
+import './styles.css';
+
+import App from './App.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
