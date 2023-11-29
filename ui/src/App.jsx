bd24c6e9d9ec5205af0895e3029865a90e6a7152
shriekster <shrieksterr@gmail.com>
2023-11-29 22:42:18 +0200

    ditched hash-based routing; using 'classic' client-side routing

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -3,11 +3,7 @@ import { useEffect } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
-import { Switch, Router, Route } from 'wouter';
-import { useLocationProperty, navigate } from 'wouter/use-location';
-import makeCachedMatcher from 'wouter/matcher';
-
-import { pathToRegexp } from 'path-to-regexp';
+import { Switch, Route } from 'wouter';
 
 import Verifier from './Verifier';
 import NotFound from './404';
@@ -21,43 +17,18 @@ const darkTheme = createTheme({
   },
 });
 
-// returns the current hash location in a normalized form
-// (excluding the leading '#' symbol)
-const hashLocation = () => window.location.hash.replace(/^#/, "") || "/";
 
-const hashNavigate = (to) => navigate("#" + to);
-
-const useHashLocation = () => {
-  const location = useLocationProperty(hashLocation);
-  return [location, hashNavigate];
-};
-
-const convertPathToRegexp = (path) => {
-  let keys = [];
-
-  // we use original pathToRegexp package here with keys
-  const regexp = pathToRegexp(path, keys, { strict: false });
-  return { keys, regexp };
-};
-
-const customMatcher = makeCachedMatcher(convertPathToRegexp);
 
 function App() {
-
-  useEffect(() => {
-
-    console.log('CHECKING')
-
-  }, []);
 
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Router hook={useHashLocation} matcher={customMatcher}>
-          <Route path='/' component={Home} />
-          <Route path='/appointments' component={Appointments} />
-          <Route path='/:other' component={NotFound} />
-      </Router>
+      <Switch>
+        <Route path='/' component={Home} />
+        <Route path='/appointments' component={Appointments} />
+        <Route path='/:anything*'component={NotFound} />
+        </Switch>
       <Verifier />
     </ThemeProvider>
   )
