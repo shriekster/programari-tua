e1c4e0b8a838d28d719bf0e528bfd350743cc4cf
shriekster <shrieksterr@gmail.com>
2024-01-23 13:37:00 +0200

    work, work

--- frontend/src/components/DateAdd.jsx
+++ frontend/src/components/DateAdd.jsx
@@ -6,51 +6,29 @@ import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
-import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import InputAdornment from '@mui/material/InputAdornment';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 import IconButton  from '@mui/material/IconButton';
 import ClearIcon from '@mui/icons-material/Clear';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+
 import PlaceIcon from '@mui/icons-material/Place';
 import CircularProgress from '@mui/material/CircularProgress';
 
-import Autocomplete from '@mui/material/Autocomplete';
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-import { matchSorter } from 'match-sorter';
-
-import Map from 'ol/Map.js';
-import OSM from 'ol/source/OSM.js';
-import TileLayer from 'ol/layer/Tile.js';
-import VectorLayer from 'ol/layer/Vector.js';
-import VectorSource from 'ol/source/Vector.js';
-import View from 'ol/View.js';
-import { Fill, Stroke, Style, Circle as CircleStyle } from 'ol/style';
-import Feature from 'ol/Feature';
-import { Point } from 'ol/geom';
-import { useGeographic } from 'ol/proj';
-import 'ol/ol.css';
 
 import { useGlobalStore } from '../useGlobalStore';
 import refreshAccessToken from '../refreshAccessToken.js';
 
-const tuaLightInnerTheme = createTheme({
-    palette: {
-        primary: {
-            main: '#7f805d'
-          },
-        mode: 'light',
-    },
-});
 
-const filterOptions = (options, { inputValue }) => matchSorter(options, inputValue, {
-    keys: ['display_name']
-});
 
 const isEmptyStringRegex = /^$/;
 
@@ -71,6 +49,29 @@ export default function DateAdd({ open, handleClose, date }) {
 
     const [activeStep, setActiveStep] = useState(-1);
 
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const openMenu = Boolean(anchorEl);
+
+    const locations = useGlobalStore((state) => state.locations);
+
+    const handleClickListItem = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleMenuItemClick = (event, index) => {
+
+      setSelectedIndex(index);
+      setAnchorEl(null);
+
+    };
+  
+    const handleCloseMenu = () => {
+
+      setAnchorEl(null);
+
+    };
+
 
     const handleGoToLastStep = () => {
 
@@ -90,7 +91,7 @@ export default function DateAdd({ open, handleClose, date }) {
 
     };
 
-    const handleSaveLocation = async () => {
+    const handleSaveDate = async () => {
 
     };
 
@@ -99,7 +100,7 @@ export default function DateAdd({ open, handleClose, date }) {
 
     }, [open]);
 
-    // create and initialize the map only when the active step is 0 (zero)
+    
     useEffect(() => {
 
     }, [open, activeStep]);
@@ -119,24 +120,72 @@ export default function DateAdd({ open, handleClose, date }) {
                 }
             </DialogTitle>
             <DialogContent sx={{ margin: '0 auto', padding: 0, position: 'relative' }}>
+              <Box sx={{
+                    height: '300px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'space-evenly'
+                }}>
                 {
                     0 === activeStep ? (
-                        <>
-                          0
-                        </>
+                      <>
+                      <Typography textAlign='center'>
+                        Selectează o locație
+                      </Typography>
+                        <List
+                          component='nav'
+                          aria-label='Locație'
+                          sx={{ 
+                            border: '1px solid rgba(255, 255, 255, .125)',
+                            borderRadius: '4px',
+                          }}
+                        >
+                          <ListItemButton
+                            id='location-button'
+                            aria-haspopup='listbox'
+                            aria-controls='location-menu'
+                            aria-label='Locație'
+                            aria-expanded={openMenu ? 'true' : undefined}
+                            onClick={handleClickListItem}
+                          >
+                            <ListItemIcon sx={{ minWidth: '40px !important' }}>
+                              <PlaceIcon />
+                            </ListItemIcon>
+                            <ListItemText
+                              primary={locations?.[selectedIndex]?.name}
+                            />
+                          </ListItemButton>
+                        </List>
+                        <Menu sx={{ width: '100%'}}
+                          id='location-menu'
+                          anchorEl={anchorEl}
+                          open={openMenu}
+                          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                          onClose={handleCloseMenu}
+                          MenuListProps={{
+                            'aria-labelledby': 'location-button',
+                            role: 'listbox',
+                          }}
+                        >
+                          {locations.map((location, index) => (
+                            <MenuItem
+                              key={location.id}
+                              selected={index === selectedIndex}
+                              onClick={(event) => handleMenuItemClick(event, index)}
+                            >
+                              {location.name}
+                            </MenuItem>
+                          ))}
+                        </Menu>
+                      </>
                     ) : (
-                        <Box sx={{
-                            height: '300px',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'space-evenly'
-                        }}>
-                            1
-                        </Box>
+                        1
                     )
 
                 }
+              </Box>
                 {
                     saving && (
                         <CircularProgress
@@ -187,7 +236,7 @@ export default function DateAdd({ open, handleClose, date }) {
                             </Button>
                             <Button variant='contained'
                                 disabled={saving}
-                                onClick={handleSaveLocation}>
+                                onClick={handleSaveDate}>
                                 Salvează
                             </Button>
                         </>
